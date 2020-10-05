5afa4a01a7ea9d6e03227b9d674834bd39caf23f
garciaveravictor <71645624+garciaveravictor@users.noreply.github.com>
2020-10-05 14:42:54 +0200

    Tarea 4.3.8 update final

--- Tema1/Tarea4.3.8/src/tarea4/pkg3/pkg8/Tarea438.java
+++ Tema1/Tarea4.3.8/src/tarea4/pkg3/pkg8/Tarea438.java
@@ -5,14 +5,18 @@
  */
 package tarea4.pkg3.pkg8;
 
-import javafx.animation.TranslateTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import com.sun.javafx.perf.PerformanceTracker;
 
 /**
  *
@@ -20,32 +24,57 @@ import javafx.util.Duration;
  */
 public class Tarea438 extends Application {
     
+    public static double bolitaSpeedX = 1;
+    public static double bolitaSpeedY = 1;
+
     @Override
     public void start(Stage primaryStage) {
         
         //Instanciar circulo
-        Circle bolita = new Circle(150, 150, 10);
-        
+        Circle bolita = new Circle(10);
+        bolita.setTranslateX(300 * 0.5);
+        bolita.setTranslateY(300 * 0.5);
+
+
         //Label
-        Label fps = new Label("FPS = ");
-        
-        //Animacion
-        TranslateTransition tt = new TranslateTransition(Duration.seconds(2));
-        tt.setNode(bolita);
-        tt.setCycleCount(300);
- 
-        //Movimiento de la bola
-        tt.toXProperty();
-        tt.toYProperty();
-              
-        tt.play();
-        
+        Label fps = new Label();
+
         Pane root = new Pane();
-        
+
         root.getChildren().add(bolita);
+        root.getChildren().add(fps);
+
+        Scene scene = new Scene(root, 400, 300);
+
+        EventHandler<ActionEvent> eH = e -> {
+            
+            // Mostrar la frecuencia de refresco FPS
+            PerformanceTracker perfTracker = PerformanceTracker.getSceneTracker(scene);
+
+            fps.setText("FPS (Timeline) = " + perfTracker.getInstantFPS());
+            
+            System.out.println(bolita.getTranslateX());
+            
+            // Cambiar la dirección de la bola si llega a los extremos
+            if (bolita.getTranslateX() < 0 || bolita.getTranslateX() > 400) {
+                bolitaSpeedX *= -1;
+            }
+           
+            if (bolita.getTranslateY() < 0 || bolita.getTranslateY() > 300) {
+                bolitaSpeedY *= -1;
+            }
+            
+            bolita.setTranslateX(bolita.getTranslateX() + bolitaSpeedX);
+            bolita.setTranslateY(bolita.getTranslateY() + bolitaSpeedY);
+            
+        };
+
+        Timeline animation = new Timeline(new KeyFrame(Duration.millis(5), eH));
+        animation.setCycleCount(Timeline.INDEFINITE);
         
-        Scene scene = new Scene(root, 300, 300);
-        
+        // iniciamos animation
+        animation.play();
+
         primaryStage.setTitle("Timeline Demo");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -57,5 +86,5 @@ public class Tarea438 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
